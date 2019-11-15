29c806121810f6aa62ac90a3c4bcbf41a031c276
Val Kobilaski <16vvk@queensu.ca>
2019-11-15 12:37:25 -0500

    Updated to make zip file

--- datascraping.sh
+++ datascraping.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-for year in `seq 1998 2008`; do 
+
+for year in `seq 2009 2019`; do 
   for month in `seq 1 12`; do 
-    wget --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=1706&Year=${year}&Month=${month}&Day=14&timeframe=1&submit= Download+Data" 
+    wget --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=47267&Year=${year}&Month=${month}&Day=14&timeframe=1&submit= Download+Data" 
     echo $year
     echo $month
     echo ""
+
   done
 done
+
+mkdir data
+
+for f in *.csv; do
+  mv $f  data/$f
+done
+
+zip -r data.zip data
+
+for f in data/*.csv; do
+  rm $f
+done
+
+rmdir data
