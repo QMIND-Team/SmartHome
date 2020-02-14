841b140ba23055dd3bf94594c2798515c760eb5d
16elp <44043175+16elp@users.noreply.github.com>
2020-02-14 15:11:03 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -31,7 +31,7 @@ for j in range 48:
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-theatreNorm = sc.fit_transform(webTempData)
+webNorm = sc.fit_transform(webTempData)
 theatreNorm = sc.fit_transform(theatreTempData)
 
 
@@ -50,13 +50,12 @@ classifier.add(Dense(1,init='uniform', activation='sigmoid', input_dim=11))
 #compile the ann
 classifier.compile(optimizer='adam', loss= 'binary_crossentropy', metrics=['accuracy'])
 
-
 # Fitting classifier to the Training set
-classifier.fit(X_train, y_train, batch_size=10, nb_epoch=100)
+classifier.fit(webNorm, theatreNorm, batch_size=10, nb_epoch=100)
 
 
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)
+y_pred = classifier.predict(theatreNorm)
 y_pred= (y_pred>0.5)
 
 new_prediction= classifier.predict(sc.transform(np.array([[0.0,0,600, 1, 40,3,60000,2,1,1,50000]])))
@@ -65,20 +64,21 @@ new_prediction=(new_prediction>0.5)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
+
 #Evaluating the ANN
-from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.cross_validation import cross_val_score
-from keras.models import Sequential
-from keras.layers import Dense
-def build_classifier():
-    classifier.add(Dense(6,init='uniform', activation='relu', input_dim=11))
-    classifier.add(Dense(6,init='uniform', activation='relu', input_dim=11))
-    classifier.add(Dense(1,init='uniform', activation='sigmoid', input_dim=11))
-    classifier.compile(optimizer='adam', loss= 'binary_crossentropy', metrics=['accuracy'])
-    return classifier
+#from keras.wrappers.scikit_learn import KerasClassifier
+#from sklearn.cross_validation import cross_val_score
+#from keras.models import Sequential
+#from keras.layers import Dense
+#def build_classifier():
+    #classifier.add(Dense(6,init='uniform', activation='relu', input_dim=11))
+    #classifier.add(Dense(6,init='uniform', activation='relu', input_dim=11))
+    #classifier.add(Dense(1,init='uniform', activation='sigmoid', input_dim=11))
+    #classifier.compile(optimizer='adam', loss= 'binary_crossentropy', metrics=['accuracy'])
+    #return classifier
     
 classifier= KerasClassifier(build_fn=build_classifier, batch_size=10, nb_epoch=100)
-accuracies= cross_val_score(estimator=classifier, X= X_train, y= y_train, cv=10, n_jobs=-1)
+accuracies= cross_val_score(estimator=classifier, X= theatreNorm, y= webNorm, cv=10, n_jobs=-1)
 
 mean=accuracies.mean()
 variance= accuracies.std()
